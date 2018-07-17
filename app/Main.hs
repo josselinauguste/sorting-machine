794f069f5d957c7ecd16ccab3479963af28e9bf0
Josselin Auguste <jauguste@iblop.net>
2018-07-17 22:22:11 +0200

    List inboxes

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
-import Lib
+import           Data.List                      ( intercalate )
+import           Lib
+import           System.Environment
 
 main :: IO ()
-main = someFunc
+main = do
+  myServer   <- imapServer
+  myUsername <- username
+  myPassword <- password
+  boxes      <- listBoxes myServer myUsername myPassword
+  putStr $ intercalate "\n" boxes
+  msgs <- getMessages myServer myUsername myPassword "[Gmail]/All Mail"
+  print $ length msgs
+
+imapServer :: IO Server
+imapServer = getEnv "IMAP_SERVER"
+
+username :: IO UserName
+username = getEnv "IMAP_USER"
+
+password :: IO Password
+password = getEnv "IMAP_PASS"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,1 +1,35 @@
-module Lib () where
+module Lib
+  ( getMessages
+  , listBoxes
+  , Server
+  , UserName
+  , Password
+  )
+where
+
+import           Network.HaskellNet.IMAP
+import           Network.HaskellNet.IMAP.SSL
+import           Network.HaskellNet.IMAP.Types
+
+type MessageUid = UID
+type Server = String
+type UserName = String
+type Password = String
+type BoxName = String
+
+getMessages :: Server -> UserName -> Password -> BoxName -> IO [MessageUid]
+getMessages server userName password boxName = do
+  conn <- connectIMAPSSL server
+  login conn userName password
+  select conn boxName
+  uids <- search conn [ALLs]
+  logout conn
+  pure uids
+
+listBoxes :: Server -> UserName -> Password -> IO [MailboxName]
+listBoxes server userName password = do
+  conn <- connectIMAPSSL server
+  login conn userName password
+  mailboxes <- list conn
+  logout conn
+  pure $ map snd mailboxes
